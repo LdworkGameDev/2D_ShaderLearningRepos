b87ee9842c0e16cc7792109ff903fb6ad43b95fd
LdworkGameDev <ldwork.gamedesign1@gmail.com>
2021-04-17 12:46:02 +0700

    Shader Dissolve 1.0

---
+++ 2D Shader Effects Project/Assets/Scripts/Mat_DissolveController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mat_DissolveController : MonoBehaviour
+{
+    public float density;
+    public float dissolveArea;
+
+    private void material;
+    private float dissolveValue;
+}
