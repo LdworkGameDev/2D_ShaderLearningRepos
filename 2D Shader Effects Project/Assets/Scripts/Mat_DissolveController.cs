fc5f4be5f165bb61d7612d45c0e88e4296616bd9
LdworkGameDev <ldwork.gamedesign1@gmail.com>
2021-04-18 11:45:15 +0700

    Shader foliage, waypointwave, simple outline glowing

--- 2D Shader Effects Project/Assets/Scripts/Mat_DissolveController.cs
+++ 2D Shader Effects Project/Assets/Scripts/Mat_DissolveController.cs
@@ -4,9 +4,69 @@ using UnityEngine;
 
 public class Mat_DissolveController : MonoBehaviour
 {
-    public float density;
     public float dissolveArea;
+    public float dissolveDensity;
 
-    private void material;
-    private float dissolveValue;
+    [ColorUsage(true, true)]
+    public Color dissolveColor;
+    public float dissolveTime;
+
+    private Material material;
+
+    private void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+
+        if (material != null)
+        {
+            material.SetColor("_DissolveColor", dissolveColor);
+            material.SetFloat("_DissolveArea", dissolveArea);
+            material.SetFloat("_Density", dissolveDensity);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            StopAllCoroutines();
+            StartCoroutine(DissolveIn());
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            StopAllCoroutines();
+            StartCoroutine(DissolveOut());
+        }
+    }
+
+    private IEnumerator DissolveIn()
+    {
+        if (material == null) yield break;
+        float dissolveTimeCounter = dissolveTime;
+
+        while(dissolveTimeCounter > 0)
+        {
+            dissolveTimeCounter -= Time.deltaTime;
+            material.SetFloat("_DissolveValue", dissolveTimeCounter / dissolveTime);            
+            yield return null;
+        }
+
+        material.SetFloat("_DissolveValue", 0);
+    }
+
+    private IEnumerator DissolveOut()
+    {
+        if (material == null) yield break;
+        float dissolveTimeCounter = 0;
+
+        while(dissolveTimeCounter < dissolveTime)
+        {
+            dissolveTimeCounter += Time.deltaTime;
+            material.SetFloat("_DissolveValue", dissolveTimeCounter / dissolveTime);
+            yield return null;
+        }
+
+        material.SetFloat("_DissolveValue", 1);
+    }
 }
---
+++ 2D Shader Effects Project/Assets/Scripts/Mat_FoliageController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mat_FoliageController : MonoBehaviour
+{
+    public Material mat_Foliage;
+
+    public float timeToAdd;
+    public Gradient color1;
+    public Gradient color2;
+
+    public Vector3 windBlowDirection;
+    public float windBlowArea;
+    public float windBlowSpeed;
+
+    private Material material;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().material = mat_Foliage;
+        material = GetComponent<SpriteRenderer>().material;
+
+        if(material != null)
+        {
+            material.SetFloat("_WindBlowArea", windBlowArea);
+            material.SetVector("_WindBlowDirection", windBlowDirection);
+            material.SetFloat("_WindBlowSpeed", windBlowSpeed);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            StopAllCoroutines();
+            StartCoroutine(ChangeColor(0f, 1f));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            StopAllCoroutines();
+            StartCoroutine(ChangeColor(1f, 0f));
+        }
+    }
+
+    private IEnumerator ChangeColor(float start, float end)
+    {
+        if (material == null) yield break;
+
+        float timeCounter;
+
+        if(start < end)
+        {
+            timeCounter = start;
+            while (timeCounter < end)
+            {
+                material.SetColor("_Color1", color1.Evaluate(timeCounter));
+                material.SetColor("_Color2", color2.Evaluate(timeCounter));
+                timeCounter += timeToAdd;
+                yield return null;
+            }
+
+            material.SetColor("_Color1", color1.Evaluate(end));
+            material.SetColor("_Color2", color2.Evaluate(end));
+        }
+        else
+        {
+            timeCounter = start;
+            while (timeCounter > end)
+            {
+                material.SetColor("_Color1", color1.Evaluate(timeCounter));
+                material.SetColor("_Color2", color2.Evaluate(timeCounter));
+                timeCounter -= timeToAdd;
+                yield return null;
+            }
+
+            material.SetColor("_Color1", color1.Evaluate(end));
+            material.SetColor("_Color2", color2.Evaluate(end));
+        }
+    }
+    
+}
